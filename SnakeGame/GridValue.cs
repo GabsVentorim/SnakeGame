393ba46ce0737c9380e086630f0625f7b7af8f4c
Gabriel Ventorim Silva <gabrielventorim@Gabriels-MacBook-Air.local>
2023-10-08 20:52:48 -0300

    First commit

---
+++ SnakeGame/GridValue.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace SnakeGame
+{
+	public enum EmptyEnumeration
+	{
+	}
+}
+
---
+++ SnakeGame/Position.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SnakeGame
+{
+	public class Position
+	{
+		public Position()
+		{
+		}
+	}
+}
+
