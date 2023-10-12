a8c27a1254d0ffedeb455838b0109f062a873aac
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-10-12 12:31:15 -0300

    Updating directions

--- SnakeGame/Direction.cs
+++ SnakeGame/Direction.cs
@@ -4,17 +4,43 @@
 
 		public readonly static Direction Left = new Direction(0, -1);
 		public readonly static Direction Right = new Direction(0, 1);
-        public readonly static Direction Up = new Direction(-1, 0);
-        public readonly static Direction Down = new Direction(1, 0);
+		public readonly static Direction Up = new Direction(-1, 0);
+		public readonly static Direction Down = new Direction(1, 0);
 
-
-        public int RowOffset {get;}
-		public int ColOffset {get;}
+		public int RowOffset { get; }
+		public int ColOffset { get; }
 
 		private Direction(int rowOffset, int colOffset)
 		{
 			RowOffset = rowOffset;
 			ColOffset = colOffset;
+		}
+
+		public Direction Opposite()
+		{
+			return new Direction(-RowOffset, -ColOffset);
+		}
+
+		public override bool Equals(Object obj)
+		{
+			return obj is Direction direction &&
+				RowOffset == direction.RowOffset &&
+				ColOffset == direction.ColOffset;
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(RowOffset, ColOffset);
+		}
+
+		public static bool operartor ==(Direction left, Direction right)
+		{
+			return EqualityComparer<Direction>.Default.Equals(left, right);
+		}
+
+        public static bool operartor !=(Direction left, Direction right)
+        {
+            return !(left == right);
         }
     }
 }
