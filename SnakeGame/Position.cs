4a3f3630aca515620f20e54b03a4afab72decfc2
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-10-12 12:30:02 -0300

    Updating position

--- SnakeGame/Position.cs
+++ SnakeGame/Position.cs
@@ -1,11 +1,42 @@
-﻿using System;
-namespace SnakeGame
+﻿namespace SnakeGame
 {
 	public class Position
 	{
-		public Position()
+		public int Row { get; }
+        public int Col { get; }
+
+        public Position(int row, int col)
 		{
+			Row = row;
+			Col = col;
 		}
+
+		public Position Translate(Direction dir)
+		{
+			return new Position(Row + dir.RowOffset, Col + dir.ColOffset);
+		}
+
+        public override bool Equals(Object obj)
+        {
+            return obj is Position position &&
+                Row == position.Row &&
+                Col == position.Col;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Row, Col);
+        }
+
+        public static bool operartor ==(Position left, Position right)
+		{
+			return EqualityComparer<Position>.Default.Equals(left, right);
+		}
+
+        public static bool operartor !=(Position left, Position right)
+        {
+            return !(left == right);
+        }
 	}
 }
 
